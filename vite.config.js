5ab338b5abd810ee85bc68659b342c37ec03fa20
henrisp <henrisp23>
2026-03-23 13:57:49 +0700

    deploy netlify

--- vite.config.js
+++ vite.config.js
@@ -3,8 +3,20 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
+  base: "/",
   server: {
     port: 3000,
     open: true,
+  },
+  build: {
+    outDir: "dist",
+    sourcemap: false,
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          vendor: ["react", "react-dom", "recharts"],
+        },
+      },
+    },
   },
 });
